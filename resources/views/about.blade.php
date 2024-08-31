924b165747a581c9d9f63d52ba017f0794cefc7c
iambeno1 <benonygabriel40@gmail.com>
2024-09-01 01:13:04 +0700

    belajar blade templating

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog | About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h1>About me</h1>
-    <img src="img/ben.jpg" alt="<?= $nama;?>" width="200px">
-    <h3><?= $nama;?></h3>
-    <b><?= $prodi;?></b>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, dolores nobis mollitia veritatis nesciunt eum ab nostrum voluptatem! Vitae iure commodi ex temporibus rem doloribus repellat modi beatae, blanditiis earum!</p>
-</body>
-</html>
+@extends("layouts.main")
+
+@section("title", "About")
+
+@section("container")
+      <div class="container">
+        <h1>About me</h1>
+        <img src="img/ben.jpg" alt="{{ $nama }}" width="200px">
+        <h3>{{ $nama }}</h3>
+        <b>{{ $prodi }}</b>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, dolores nobis mollitia veritatis nesciunt eum ab nostrum voluptatem! Vitae iure commodi ex temporibus rem doloribus repellat modi beatae, blanditiis earum!</p>
+      </div>
+@endsection
+
