7f81faff0369f544fb1049ea0c5392352876b8bf
iambeno1 <benonygabriel40@gmail.com>
2024-08-31 22:04:38 +0700

    belajar struktur folder, routes, dan view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>About me</h1>
+    <img src="img/ben.jpg" alt="Benony" width="200px">
+    <h3>Benony Gabriel</h3>
+    <b>Ilmu Komputer</b>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, dolores nobis mollitia veritatis nesciunt eum ab nostrum voluptatem! Vitae iure commodi ex temporibus rem doloribus repellat modi beatae, blanditiis earum!</p>
+</body>
+</html>
