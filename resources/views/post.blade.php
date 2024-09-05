c03cc0238c1ebfdabb4335b13fc3dc595f76a7fe
iambeno1 <benonygabriel40@gmail.com>
2024-09-06 00:30:04 +0700

    Post model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,9 +7,9 @@
 <a href="/blog">Back</a>
 
     <article class="mt-5">
-        <h2>{{ $post["title"] }}</a></h2>
-        <h6>By: {{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+        <h2>{{ $post->title }}</a></h2>
+        <h6>By: {{ $post->author }}</h5>
+        {{ $post->body }}
     </article>
 
     
